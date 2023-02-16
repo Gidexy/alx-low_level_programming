1e96ab54188b55d797199c10d88a64267dcd05ea
Gidexy <gideonigboss2@gmail.com>
2023-02-16 01:06:42 -0800

    first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ *The main function is to print a line of code using put
+ *it should then return 0
+ */
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+}
