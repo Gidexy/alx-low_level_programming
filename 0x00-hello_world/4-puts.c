abd51f7d5ad28aa861e34a3cf9c49ac37c5d3e71
Gidexy <gideonigboss2@gmail.com>
2023-02-16 01:20:35 -0800

    last check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  *main-function is to print a line of code usi ng puts
  *Return: 0
  */
-int main (void)
+int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+  return (0);
 }
