6a802b8ce035d19f78db375901a3d56816e60e19
Gidexy <gideonigboss2@gmail.com>
2023-02-21 15:15:25 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10- prints small letter alphabets
+ *
+ *Return: 0
+ */
+
+print_alphabet_x10(void)
+{
+	int r;
+
+	r = 0;
+
+	while (r < 10)
+	{
+		char ch;
+
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+
+	r++;
+	}
+}
