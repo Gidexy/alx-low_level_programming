09adda95416db0f3955867d95112bfeac06d9b3f
Gidexy <gideonigboss2@gmail.com>
2023-02-26 20:17:20 -0800

    way

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,13 +5,12 @@
 * Return: 0
 */
 
-void positive_or_negative(void)
+int main(void)
 {
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-	return 0;
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
 }
