7c1816f3e576ef9184a33fae9b80605203235e9c
Gidexy <gideonigboss2@gmail.com>
2023-03-10 02:43:55 -0800

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- main functiin
+ *@argc: argument count
+ *@argv: argument array
+ * Return: return 0
+**/
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc > 1)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
