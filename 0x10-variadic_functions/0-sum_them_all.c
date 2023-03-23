4ee1a0c311a43c0e488c6e41decc17ae0f67a38a
Gidexy <gideonigboss2@gmail.com>
2023-03-23 06:26:11 -0700

    another sum try

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,15 +11,17 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	unsigned int i, sum = 0;
 
+	/* validate valist and initialize */
 	if (n == 0)
 		return (0);
-
 	va_start(ap, n);
 
+	/* iterate through list, update sum, free list */
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
 	}
 	va_end(ap);
+
 	return (sum);
 }
